42876aebba1d750fb24a691e83be4b11dc55cb62
Azka azeem <azkaazeem804@gmail.com>
2026-07-31 11:31:10 +0500

    eVERY THING DONE now add the real movies thumbnails and content

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ arrows.forEach((arrow, i) => {
     let clickCounter = 0;
 
     arrow.addEventListener("click", () => {
-        const ratio = Math.floor(window.innerWidth / 270);
+        const carouselWidth = arrow.parentElement.clientWidth;
+        const ratio = Math.floor(carouselWidth / 300);
         clickCounter++;
         if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
             movieLists[i].style.transform = `translateX(${movieLists[i].computedStyleMap().get("transform")[0].x.value - 300}px)`;
